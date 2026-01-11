975b91df5c48e4c6f3bc65c6bf79fa78770bb3dc
promt <wisanuphorm.pothito@gmail.com>
2026-01-12 03:38:24 +0700

    feat: first commit web socket

---
+++ server.ts
@@ -0,0 +1,23 @@
+import { WebSocketServer } from "ws";
+
+const PORT = Number(process.env.PORT) || 3001;
+
+const wss = new WebSocketServer({ port: PORT });
+
+wss.on("connection", (ws, req) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    wss.clients.forEach((client) => {
+      if (client.readyState === client.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+
+  ws.on("close", () => {
+    console.log("Client disconnected");
+  });
+});
+
+console.log("WebSocket running on port", PORT);
